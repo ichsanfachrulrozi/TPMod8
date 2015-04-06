b090fa91950ef5f8aa690c37ff53b7e5fa7b7c04
ichsanfachrulrozi <rezky.paranata@gmail.com>
2015-04-07 00:50:25 +0700

    versi pertama

---
+++ src/tpmod8/TPMod8.java
@@ -0,0 +1,16 @@
+
+
+package tpmod8;
+
+
+public class TPMod8 {
+
+        public static void main(String[] args) {
+        String nama="Ichsan Fachrul Rozi.";
+        String nim="6306130017.";
+       
+        System.out.println(" Hello, saya " + nama + " Nim saya " + nim + " Saya sangat menyukai praktikum RPL Modul 8 ini. " );
+    }
+    
+    
+}
